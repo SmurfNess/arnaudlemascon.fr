6f3ea5893a6aa58e62e33152bcfa56eee7588a63
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-02-03 23:36:46 +0100

    Update index.php

--- projects/cv/index.php
+++ projects/cv/index.php
@@ -267,7 +267,7 @@ try {
   position: relative;
   padding: 10px;
   border: none;
-  background: linear-gradient(145deg, rgba(166, 179, 255, 0.5) 5%,rgba(255,255,255,0) 80%);
+  background: linear-gradient(145deg, rgba(166, 179, 255, 1) 5%,rgba(255,255,255,0) 80%);
   border-radius: 100px 0px 0px 100px;
   margin-top:10px;
   width: 100%;
