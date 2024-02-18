1bf3edbb07b110d7dad84f3b368cc39e32c6714e
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-02-18 17:37:53 +0100

    Update account.php

--- projects/quiz/account.php
+++ projects/quiz/account.php
@@ -86,6 +86,7 @@ if (isset($_SESSION['user_type'])) {
                     width: 400px;
                     height: 400px;
                     display: inline-block;
+                    position: relative;
                 }
 
                 .card img {
@@ -93,22 +94,76 @@ if (isset($_SESSION['user_type'])) {
                     height: 300px;
                     object-fit: cover;
                 }
+
+                .card .button-container {
+                    position: absolute;
+                    bottom: 10px;
+                    left: 10px;
+                }
+
+                .card .button-container button {
+                    margin-right: 5px;
+                }
+
+                .card .score {
+                    position: absolute;
+                    top: 10px;
+                    left: 10px;
+                    background-color: rgba(255, 0, 0, 0.7);
+                    padding: 5px;
+                    color: white;
+                    border-radius: 5px;
+                    display: none;
+                }
             </style>
         </head>
         <body>
             <div class="container">
+                <div class="count">Votre score est de <span id="score">0</span></div>
                 <?php foreach ($cards as $card): ?>
                     <div class="card">
+                        <div class="score"></div>
                         <div class="card-number"><?php echo $card['number']; ?></div>
                         <h2 class="card-title"><?php echo $card['Question']; ?></h2>
                         <img src="<?php echo $card['Image']; ?>" alt="Image">
-                        <button><?php echo $card['Answer']; ?></button>
-                        <button><?php echo $card['prop1']; ?></button>
-                        <button><?php echo $card['prop2']; ?></button>
-                        <button><?php echo $card['prop3']; ?></button>
+                        <div class="button-container">
+                            <button class="answer"><?php echo $card['Answer']; ?></button>
+                            <button class="prop"><?php echo $card['prop1']; ?></button>
+                            <button class="prop"><?php echo $card['prop2']; ?></button>
+                            <button class="prop"><?php echo $card['prop3']; ?></button>
+                        </div>
                     </div>
                 <?php endforeach; ?>
             </div>
+
+            <script>
+                var score = 0;
+
+                document.querySelectorAll('.card').forEach(function(card) {
+                    var buttons = card.querySelectorAll('.button-container button');
+                    var answerButton = card.querySelector('.answer');
+                    var scoreDisplay = card.querySelector('.score');
+
+                    buttons.forEach(function(button) {
+                        button.addEventListener('click', function() {
+                            buttons.forEach(function(btn) {
+                                btn.disabled = true;
+                            });
+
+                            if (button === answerButton) {
+                                score++;
+                                scoreDisplay.textContent = "+1";
+                            } else {
+                                scoreDisplay.textContent = "Mauvaise réponse";
+                            }
+
+                            scoreDisplay.style.display = 'block';
+                        });
+                    });
+                });
+
+                document.getElementById('score').textContent = score;
+            </script>
         </body>
         </html>
 
