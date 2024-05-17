937653c00c9e34d2ea7f98691d36dc0209327418
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-05-18 01:54:50 +0200

    Update account.php

--- projects/TeamUp/account.php
+++ projects/TeamUp/account.php
@@ -10,19 +10,6 @@ if (isset($_SESSION['user_type'])) {
         $login_username = $_SESSION['login_username'];
 
         if ($user_type == $admin) {
-            // Afficher le formulaire pour ajouter des joueurs
-            ?>
-            <form method="post" action="team.php">
-                <label for="name">Nom :</label>
-                <input type="text" id="name" name="name" required><br><br>
-                
-                <label for="class">Classe :</label>
-                <input type="text" id="class" name="class" required><br><br>
-                
-                <input type="submit" value="Envoyer">
-            </form>
-            <?php
-        } elseif ($user_type == $util) {
             try {
                 $connexion = new PDO("mysql:host={$databaseConfig['server']};dbname={$databaseConfig['database']}", $databaseConfig['username'], $databaseConfig['password']);
                 $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -56,14 +43,21 @@ if (isset($_SESSION['user_type'])) {
             </html>
 
             <?php
-        } else { 
+        } elseif ($user_type == $util) {
+            // Si l'utilisateur est un utilisateur ordinaire, afficher un message de bienvenue
             echo "<h1>Bienvenue..</h1>";
+        } else {
+            // Si le type d'utilisateur n'est ni admin ni utilisateur, rediriger vers la page de connexion
+            header("Location: teamup.html");
+            exit();
         }
     } else {
+        // Si l'utilisateur n'est pas connecté, rediriger vers la page de connexion
         header("Location: teamup.html");
         exit();
     }
 } else {
+    // Si le type d'utilisateur n'est pas défini, rediriger vers la page de connexion
     header("Location: teamup.html");
     exit();
 }
