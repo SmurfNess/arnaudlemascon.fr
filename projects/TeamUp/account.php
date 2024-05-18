f031a5e8768126a6afadf5ac95cae9deb68a3ebb
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-05-18 02:01:00 +0200

    Update account.php

--- projects/TeamUp/account.php
+++ projects/TeamUp/account.php
@@ -28,7 +28,7 @@ if (isset($_SESSION['user_type'])) {
                 $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
                 // Sélectionner les joueurs de l'utilisateur connecté
-                $query = "SELECT name, class, team FROM player WHERE owner = :owner";
+                $query = "SELECT name, class, team FROM players WHERE owner = :owner";
                 $stmt = $connexion->prepare($query);
                 $stmt->bindParam(':owner', $login_username);
                 $stmt->execute();
@@ -59,6 +59,23 @@ if (isset($_SESSION['user_type'])) {
         } elseif ($user_type == $util) {
             // Si l'utilisateur est un utilisateur ordinaire, afficher un message de bienvenue
             echo "<h1>Bienvenue..</h1>";
+            <?php
+
+            try {
+                $connexion = new PDO("mysql:host={$databaseConfig['server']};dbname={$databaseConfig['database']}", $databaseConfig['username'], $databaseConfig['password']);
+                $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Sélectionner les joueurs de l'utilisateur connecté
+                $query = "SELECT name, class, team FROM players WHERE owner = :owner";
+                $stmt = $connexion->prepare($query);
+                $stmt->bindParam(':owner', $login_username);
+                $stmt->execute();
+                $players = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                echo "Erreur de connexion : " . $e->getMessage();
+            }
+            ?>
+
         } else {
             // Si le type d'utilisateur n'est ni admin ni utilisateur, rediriger vers la page de connexion
             header("Location: teamup.html");
