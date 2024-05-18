48196ba88676aa1d9186770f0fd4f225af145f32
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-05-18 21:09:58 +0200

    teamup

--- projects/TeamUp/team.php
+++ projects/TeamUp/team.php
@@ -124,6 +124,7 @@ if (isset($_SESSION['user_type'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Équipe</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Gestion des joueurs et des équipes</h1>
