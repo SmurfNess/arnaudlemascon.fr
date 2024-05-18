58bfe564cd354090a6f023285324b33b80d3d1d3
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-05-18 21:30:11 +0200

    Update team.php

--- projects/TeamUp/team.php
+++ projects/TeamUp/team.php
@@ -134,7 +134,7 @@ if (isset($_SESSION['user_type'])) {
 </head>
 <body>
     <div class="row">
-    <div class="col-6 mb-2">
+    <div class="col-6 m-2">
     <h1>Gestion des joueurs et des équipes</h1>
 
     <section>
@@ -151,7 +151,7 @@ if (isset($_SESSION['user_type'])) {
     </section>
     </div>
     
-    <div class="col-6 mb-2">
+    <div class="col-6 m-2">
     <section>
         <h2>Générer les équipes</h2>
         <form method="post" action="team.php">
@@ -173,7 +173,7 @@ if (isset($_SESSION['user_type'])) {
     </div>
 
     <div class="row">
-    <div class="col-6 mb-2">
+    <div class="col-6 m-2">
     <section>
     <h2>Résultat de la génération d'équipes</h2>
     <table>
@@ -199,7 +199,7 @@ if (isset($_SESSION['user_type'])) {
 </section>
 </div>
     
-<div class="col-6 mb-2">
+<div class="col-6 m-2">
 <section>
     <h2>Liste complète des joueurs</h2>
     <table>
