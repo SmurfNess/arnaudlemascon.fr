770789744f6149caf6f806788ba7b00c00a4b6a5
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-06-15 00:33:35 +0200

    Update index.php

--- projects/cv/index.php
+++ projects/cv/index.php
@@ -247,7 +247,8 @@ try {
     max-width: 800px; /* Set a max-width to the container */
     margin: auto; /* Center the container horizontally */
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border-radius: 8px red solid;
+    border-radius: 8px;
+    background-color:red;
     overflow: hidden;
 }
 
