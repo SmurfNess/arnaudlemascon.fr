658a42f90bdea3076fbef8e262269a05f86e2096
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-09-09 21:30:56 +0200

    Update contentGeneration.js

--- assets/js/contentGeneration.js
+++ assets/js/contentGeneration.js
@@ -1,19 +1,17 @@
 let data = {}; // This will hold your JSON data
 let currentLanguage = 'en'; // Default language
 
-// Fetch JSON data from the server
 async function fetchData() {
     try {
         const response = await fetch('https://arnaudlemascon.fr/assets/json/data.json');
         data = await response.json();
-        console.log('Data loaded:', data); // Debugging
+        console.log('Data loaded:', data); // Ajoutez ce log
         generateContent();
     } catch (error) {
         console.error('Error fetching data:', error);
     }
 }
 
-// Generate all content sections
 function generateContent() {
     generateArticle();
     generateProjects();
@@ -21,35 +19,21 @@ function generateContent() {
     generateSkills();
 }
 
-// Change language and regenerate content
 function changeLanguage(language) {
     currentLanguage = language;
     generateContent();
 }
 
-// Generate articles and place them in their respective sections
 function generateArticle() {
-    console.log('Generating articles...');
+    console.log('Generating articles...'); // Ajoutez ce log
     const sections = {
-        'HOME': document.querySelector('#HOME .article-container'),
-        'PROJECTS': document.querySelector('#PROJECTS .article-container'),
-        'VALUES': document.querySelector('#VALUES .article-container'),
-        'SKILLS': document.querySelector('#SKILLS .article-container'),
+        'HOME': document.querySelector('#HOME #article-container'),
+        'SKILLS': document.querySelector('#SKILLS .skills-container'),
     };
 
-    console.log('Sections:', sections);
-
     if (data.Article && Array.isArray(data.Article)) {
-        console.log('Articles:', data.Article);
-
-        // Clear all article containers
-        Object.values(sections).forEach(container => {
-            if (container) {
-                container.innerHTML = ''; // Clear the container
-            } else {
-                console.warn('Container not found.');
-            }
-        });
+        console.log('Articles:', data.Article); // Ajoutez ce log
+        Object.values(sections).forEach(container => container.innerHTML = '');
 
         data.Article.forEach(article => {
             const container = sections[article.section];
@@ -66,23 +50,19 @@ function generateArticle() {
             }
         });
     } else {
-        // Handle case where there are no articles
         Object.values(sections).forEach(container => {
-            if (container) {
-                container.innerHTML = '<p>No articles available.</p>';
-            }
+            container.innerHTML = '<p>No articles available.</p>';
         });
     }
 }
 
-// Generate project items
 function generateProjects() {
     const container = document.querySelector('#PROJECTS .project-container');
-    if (container) {
-        container.innerHTML = ''; // Clear the container
+    container.innerHTML = '';
 
+    if (data.projects && Array.isArray(data.projects)) {
         data.projects.forEach(project => {
-            const technoHTML = project.techno.map(techno => `<div class="techno-label" id="${techno}">${techno}</div>`).join('');
+            const technoHTML = project.techno ? project.techno.map(techno => `<div class="techno-label" id="${techno}">${techno}</div>`).join('') : '';
 
             const projectHTML = `
                 <div class="cards">
@@ -102,16 +82,15 @@ function generateProjects() {
             container.insertAdjacentHTML('beforeend', projectHTML);
         });
     } else {
-        console.warn('Project container not found.');
+        container.innerHTML = '<p>No projects available.</p>';
     }
 }
 
-// Generate value items
 function generateValues() {
     const container = document.querySelector('#VALUES .container-values .row');
-    if (container) {
-        container.innerHTML = ''; // Clear the container
+    container.innerHTML = ''; // Clear the container
 
+    if (data.values && Array.isArray(data.values)) {
         data.values.forEach(value => {
             const valueHTML = `
                 <div class="col-6 col-md-4 mb-4">
@@ -128,61 +107,74 @@ function generateValues() {
                     </div>
                 </div>
             `;
-
+            
             container.insertAdjacentHTML('beforeend', valueHTML);
         });
     } else {
-        console.warn('Values container not found.');
+        container.innerHTML = '<p>No values available.</p>';
     }
 }
 
-// Generate skills items
 function generateSkills() {
     const container = document.querySelector('#SKILLS .skills-container');
-    if (container) {
-        container.innerHTML = ''; // Clear the container
+    container.innerHTML = ''; // Clear the container
 
-        // Categorize skills by type
-        const skillTypes = {
-            'development': [],
-            'language': []
-        };
+    // Group skills by type
+    const groupedSkills = {
+        'OS': [],
+        'DEV': [],
+        'Langues': []
+    };
 
-        // Sort skills into categories
-        for (const key in data.skills) {
+    for (const key in data.skills) {
+        if (data.skills.hasOwnProperty(key)) {
             const skill = data.skills[key];
-            skillTypes[skill.type].push(skill);
-        }
-
-        // Generate HTML for each skill type
-        for (const [type, skills] of Object.entries(skillTypes)) {
-            let typeTitle;
-            if (type === 'development') {
-                typeTitle = 'DEV';
-            } else if (type === 'language') {
-                typeTitle = 'Langues';
+            if (skill.type === 'development') {
+                // Assuming you categorize based on a specific set of skill types
+                groupedSkills['DEV'].push(skill);
+            } else if (skill.type === 'language') {
+                groupedSkills['Langues'].push(skill);
+            } else {
+                // You can add more categories here if needed
+                groupedSkills['OS'].push(skill);
             }
-
-            const typeHTML = `
-                <div class="col-2 card_skills">
-                    <div class="card_skills-type">${typeTitle}</div>
-                    ${skills.map(skill => `
-                        <div class="gauge-wrapper">
-                            ${skill.name[currentLanguage]}
-                            <div class="gauge">
-                                <div class="gauge-level" style="width:${skill.level}%"></div>
-                            </div>
-                        </div>
-                    `).join('<div class="separator"></div>')}
-                </div>
-            `;
-
-            container.insertAdjacentHTML('beforeend', typeHTML);
         }
-    } else {
-        console.warn('Skills container not found.');
     }
+
+    // Function to create HTML for a skill group
+    const createSkillGroupHTML = (type, skills) => {
+        if (skills.length === 0) return '';
+
+        const skillsHTML = skills.map(skill => `
+            <div class="gauge-wrapper">
+                ${skill.name[currentLanguage]}
+                <div class="gauge">
+                    <div class="gauge-level" style="width:${skill.level}%"></div>
+                </div>
+            </div>
+        `).join('<div class="separator"></div>');
+
+        return `
+            <div class="col-2 card_skills">
+                <div class="card_skills-type">${type}</div>
+                ${skillsHTML}
+            </div>
+        `;
+    };
+
+    // Generate HTML for each skill group
+    const osSkillsHTML = createSkillGroupHTML('OS', groupedSkills['OS']);
+    const devSkillsHTML = createSkillGroupHTML('DEV', groupedSkills['DEV']);
+    const languageSkillsHTML = createSkillGroupHTML('Langues', groupedSkills['Langues']);
+
+    // Combine all skill groups and insert into container
+    container.innerHTML = `
+        <div class="row">
+            ${osSkillsHTML}
+            ${devSkillsHTML}
+            ${languageSkillsHTML}
+        </div>
+    `;
 }
 
-// Fetch data when the script is loaded
 fetchData();
