e87c50e35742426c77a364f9e31ca3ae3d3ff048
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-10-13 09:35:20 +0200

    up

--- assets/js/contentGeneration.js
+++ assets/js/contentGeneration.js
@@ -296,7 +296,7 @@ function generateContactForm() {
     // Générer le HTML du formulaire
     const contactHTML = `
             <form action="https://formspree.io/f/xdovyzdp" method="POST">
-                <label class="col-12" >Ton nom et tes pronoms :<br>
+                <label class="col-12" style="justify-content: center;">Ton nom et tes pronoms :<br>
                     <input type="name" name="name" required>
                 </label>
                 <label class="col-12" for="email">${contactSection.expeditor[currentLanguage]}<br>
