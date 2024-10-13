8defee58bd377d3e93ce73d9ee609992181cdf1e
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-10-13 11:44:56 +0200

    Update contentGeneration.js

--- assets/js/contentGeneration.js
+++ assets/js/contentGeneration.js
@@ -146,11 +146,11 @@ function generateArticle() {
                 if (sectionName === 'PROJECTS' || sectionName === 'VALUES' || sectionName === 'SKILLS') {
                     const svgElements = section.querySelectorAll('svg'); // Select all SVG elements in the section
                     svgElements.forEach(svg => {
-                        // Set the vertical position of the SVG to match the article-container
+                        // Set the vertical position of the SVG to match the article-container minus 250px
                         svg.style.position = 'absolute'; // Ensure SVGs can be positioned absolutely
-                        svg.style.top = `${positionFromTop}px`; // Align the top of SVG with article-container
+                        svg.style.top = `${positionFromTop - 250}px`; // Align the top of SVG with article-container minus 250px
                         svg.style.left = '0'; // Set left position to avoid horizontal shift
-                        console.log(`Adjusted position of SVG in ${sectionName} to top: ${positionFromTop}px`);
+                        console.log(`Adjusted position of SVG in ${sectionName} to top: ${positionFromTop - 250}px`);
                     });
                 }
             } else {
