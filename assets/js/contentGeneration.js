db563212499899836b0114247832132c04ca4356
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-10-15 13:03:32 +0200

    Update contentGeneration.js

--- assets/js/contentGeneration.js
+++ assets/js/contentGeneration.js
@@ -269,7 +269,7 @@ function generateValues() {
 
         data.values.forEach(value => {
             const valueHTML = `
-                <div class="col-4" style="background-color:red;">
+                <div class="col-4" style="background-color:red;border:black solid 2px;">
                     <div class="carte">
                         <div class="carte-inner">
                             <div class="face face-avant">
