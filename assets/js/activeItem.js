2b99192e34a14642b984eca240c46547e2df02e5
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-11-10 17:32:38 +0100

    Update activeItem.js

--- assets/js/activeItem.js
+++ assets/js/activeItem.js
@@ -4,15 +4,67 @@ function closeNavbar() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-const navLinks = document.querySelectorAll('.nav-link');
+dvar nav = $('nav');
+var line = $('<div />').addClass('line');
 
-function setActiveLink(event) {
- navLinks.forEach(link => {
-     link.classList.remove('active');
- });
- event.target.classList.add('active');
+line.appendTo(nav);
+
+var active = nav.find('.active');
+var pos = 0;
+var wid = 0;
+
+if(active.length) {
+  pos = active.position().left;
+  wid = active.width();
+  line.css({
+    left: pos,
+    width: wid
+  });
 }
+
+nav.find('ul li a').click(function(e) {
+  e.preventDefault();
+  if(!$(this).parent().hasClass('active') && !nav.hasClass('animate')) {
+    
+    nav.addClass('animate');
+
+    var _this = $(this);
+
+    nav.find('ul li').removeClass('active');
+
+    var position = _this.parent().position();
+    var width = _this.parent().width();
+
+    if(position.left >= pos) {
+      line.animate({
+        width: ((position.left - pos) + width)
+      }, 300, function() {
+        line.animate({
+          width: width,
+          left: position.left
+        }, 150, function() {
+          nav.removeClass('animate');
+        });
+        _this.parent().addClass('active');
+      });
+    } else {
+      line.animate({
+        left: position.left,
+        width: ((pos - position.left) + wid)
+      }, 300, function() {
+        line.animate({
+          width: width
+        }, 150, function() {
+          nav.removeClass('animate');
+        });
+        _this.parent().addClass('active');
+      });
+    }
+
+    pos = position.left;
+    wid = width;
+  }
+});
 
 navLinks.forEach(link => {
  link.addEventListener('click', function(event) {
