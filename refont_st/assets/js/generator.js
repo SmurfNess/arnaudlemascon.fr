49db2af59fb32f7fea7198e6fc702f614d1b9909
KvotheGreatNess <arnaud.lemascon@gmail.com>
2024-12-29 15:32:21 +0100

    Update generator.js

--- refont_st/assets/js/generator.js
+++ refont_st/assets/js/generator.js
@@ -306,8 +306,6 @@ document.addEventListener('DOMContentLoaded', function () {
             currentEntDiv.innerHTML = `
                 ${CurrentEnt}
             `;
-            console.log('${CurrentEnt}');
-            console.log('${mostRecentPosition.enterprise}');
         }
     }
     
